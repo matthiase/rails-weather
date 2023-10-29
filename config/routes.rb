17e08d7ad370cf865d5d18d228fc731fcec75f66
Matthias Eder <nodeful@gmail.com>
2023-10-30 00:08:40 +0100

    Add very basic tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get 'weather', to: 'weather#info'
-  get 'weather/:id', to: 'weather#info'
+  get 'weather', to: 'weather#info', as: :weather_index
+  get 'weather/:id', to: 'weather#info', as: :weather_info
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
--- test/controllers/weather_controller_test.rb
+++ test/controllers/weather_controller_test.rb
@@ -1,8 +1,13 @@
-require "test_helper"
+require 'test_helper'
 
 class WeatherControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get weather_index_url
+    assert_response :success
+  end
+
+  test 'should get info' do
+    get weather_info_url(id: 'test')
     assert_response :success
   end
 end
